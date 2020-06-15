8b8c9e03961ffd30de063266828fd25abbf6af66
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-14 21:39:53 -0500

    Add project files.

---
+++ Controllers/MoiveHomeController.cs
@@ -0,0 +1,23 @@
+﻿using cis174projects.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace cis174projects.Controllers
+{
+    public class MovieHomeController : Controller
+    {
+        private MovieContext context { get; set; }
+
+        public MovieHomeController(MovieContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = context.Movies.Include(m => m.Genre).OrderBy(m => m.Name).ToList();
+            return View(movies);
+        }
+    }
+}
