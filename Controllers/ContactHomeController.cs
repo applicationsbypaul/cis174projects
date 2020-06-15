8b3102187182628c9c4372e3ef79ac7b553f7c5c
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-14 22:23:30 -0500

    finished the index and controller to load in the contacts

---
+++ Controllers/ContactHomeController.cs
@@ -0,0 +1,20 @@
+﻿using cis174projects.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace cis174projects.Controllers
+{
+    public class ContactHomeController : Controller
+    {
+        private ContactContext context { get; set; }
+        public ContactHomeController(ContactContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            var contacts = context.Contacts.OrderBy(m => m.Name).ToList();
+            return View(contacts);
+        }
+    }
+}
