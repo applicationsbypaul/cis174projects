4c3c93d95e20cc6c6e2ea69a0c917b10044952fd
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-06-18 21:51:53 -0500

    Finished added the first responsive web app to the project list also have the test completed.

---
+++ Controllers/PersonHomeController.cs
@@ -0,0 +1,38 @@
+﻿using cis174projects.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cis174projects.Controllers
+{
+    public class PersonHomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "";
+            ViewBag.Age = "";
+            return View();
+        }
+        /// <summary>
+        /// Comfirms that the input is valid
+        /// if not, set the variables to default. 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Index(Person model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Name = model.Name;
+                ViewBag.Age = model.CalculateAge();
+            }
+            else
+            {
+                ViewBag.Name = "";
+                ViewBag.Age = "";
+            }
+            return View(model);
+        }
+    }
+}
+
---
+++ Models/Person.cs
@@ -0,0 +1,39 @@
+﻿/**************************************************************
+* Name        : Person
+* Author      : Paul Ford
+* Created     : 5/30/2000
+* Course      : CIS 174 Advance c#
+* OS          : Windows 10
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : Person class holds the attributes and the methods
+*               needing to calculate the age.
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or 
+* unmodified. I have not given other fellow student(s) access to
+* my program.
+***************************************************************/
+using System;
+using System.ComponentModel.DataAnnotations;
+namespace cis174projects.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter the year that you were born in.")]
+        [Range(1900, 2019, ErrorMessage = "Year of birth must be between 1900 and 2019")]
+        public int? BirthYear { get; set; }
+
+        public const int CurrentYear = 2020;
+
+        /// <summary>
+        /// Calculates the Age by 2020 DEC 31st
+        /// </summary>
+        /// <returns></returns>
+        public int CalculateAge()
+        {
+            return CurrentYear - BirthYear.Value;
+        }
+    }
+}
